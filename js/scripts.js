6971d6c66b0487ed571afb9c0e178549589d70ea
bncarey42 <bncarey42@gmail.com>
2018-06-17 16:17:36 -0500

    updated contact and 404 updated test content on index and fixed toggle index content

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,8 @@ function toggle_splash_text(){
   if(document.getElementById("splash_msg").style.display == "block"){
      document.getElementById("splash_msg").style.display = "none";
      document.getElementById("splash_about").style.display = "block";
+  } else {
+    document.getElementById("splash_about").style.display = "none";
+    document.getElementById("splash_msg").style.display = "block";
   }
 }
