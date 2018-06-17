472bf87fe7bcee263df7e90c31dcd13baa1f3614
bncarey42 <bncarey42@gmail.com>
2018-06-17 14:52:05 -0500

    added triangles and updated js to turn them on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-function toggle_text(id){
+function toggle_resume_text(id){
+  arrow_right_style="width: 0; height: 0; border-top: 10px solid transparent; border-bottom: 10px solid transparent; border-left: 10px solid var(--accent-2);"
+  var arrow_down_style= "width: 0; height: 0; border-left: 10px solid transparent; border-right: 10px solid transparent; border-top: 10px solid var(--accent-2);"
   if(document.getElementById(id).style.display == "none"){
      document.getElementById(id).style.display = "block"
+     document.getElementById("arrow-" + id).style = arrow_down_style;
   } else {
     document.getElementById(id).style.display = "none";
+     document.getElementById("arrow-" + id).style = arrow_right_style;    
   }
 }
 
