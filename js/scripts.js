dcd2407825286459f647e747bdc113d3920715a0
bncarey42 <bncarey42@gmail.com>
2018-06-17 15:46:43 -0500

    fixed toggle triangles inline possitioning with table

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 function toggle_resume_text(id){
-  arrow_right_style="width: 0; height: 0; border-top: 10px solid transparent; border-bottom: 10px solid transparent; border-left: 10px solid var(--accent-2);"
-  var arrow_down_style= "width: 0; height: 0; border-left: 10px solid transparent; border-right: 10px solid transparent; border-top: 10px solid var(--accent-2);"
+  arrow_right_style="width: 0; height: 0; border-top: 5px solid transparent; border-bottom: 5px solid transparent; border-left: 5px solid var(--accent-2);"
+  var arrow_down_style= "width: 0; height: 0; border-left: 5px solid transparent; border-right: 5px solid transparent; border-top: 5px solid var(--accent-2);"
   if(document.getElementById(id).style.display == "none"){
      document.getElementById(id).style.display = "block"
      document.getElementById("arrow-" + id).style = arrow_down_style;
   } else {
     document.getElementById(id).style.display = "none";
-     document.getElementById("arrow-" + id).style = arrow_right_style;    
+     document.getElementById("arrow-" + id).style = arrow_right_style;
   }
 }
 
