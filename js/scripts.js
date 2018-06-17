f075592900f5e1f5657c677fb6ce24fb2f63d3db
bncarey42 <bncarey42@gmail.com>
2018-06-17 01:27:31 -0500

    added scripts finalized design updated resume

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function toggle_text(id){
+  if( document.getElementById(id).style.display == "none"){
+     document.getElementById(id).style.display = "block"
+  } else {
+    document.getElementById(id).style.display = "none";
+  }
+}
+
+var splash_text = "<p>Welcome! click to learn more about Ben</p>";
