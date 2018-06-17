5271698cea328b16e040feb3b36dcf9dfc59c7a1
bncarey42 <bncarey42@gmail.com>
2018-06-17 02:06:51 -0500

    added toggle on index and hide elements on resume

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 function toggle_text(id){
-  if( document.getElementById(id).style.display == "none"){
+  if(document.getElementById(id).style.display == "none"){
      document.getElementById(id).style.display = "block"
   } else {
     document.getElementById(id).style.display = "none";
   }
 }
 
-var splash_text = "<p>Welcome! click to learn more about Ben</p>";
+function toggle_splash_text(){
+  if(document.getElementById("splash_msg").style.display == "block"){
+     document.getElementById("splash_msg").style.display = "none";
+     document.getElementById("splash_about").style.display = "block";
+  }
+}
