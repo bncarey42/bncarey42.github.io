1fa488e37f8aebdf4b939c44626f5d8b4435440a
bncarey42 <bncarey42@gmail.com>
2018-06-19 23:11:34 -0500

    updated to use unicode triangle pos

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 function toggle_resume_text(id){
-  arrow_right_style="width: 0; height: 0; border-top: 5px solid transparent; border-bottom: 5px solid transparent; border-left: 5px solid var(--accent-2);"
-  var arrow_down_style= "width: 0; height: 0; border-left: 5px solid transparent; border-right: 5px solid transparent; border-top: 5px solid var(--accent-2);"
   if(document.getElementById(id).style.display == "none"){
      document.getElementById(id).style.display = "block"
-     document.getElementById("arrow-" + id).style = arrow_down_style;
+     document.getElementById("arrow-" + id).innerHTML = "▼";
   } else {
     document.getElementById(id).style.display = "none";
-     document.getElementById("arrow-" + id).style = arrow_right_style;
+     document.getElementById("arrow-" + id).innerHtml = "▶";
   }
 }
 
