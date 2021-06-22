a445dc67ed1c94c993163e9d8ec13b69719f9e06
ben <bncarey42@users.noreply.github.com>
2021-06-21 19:55:09 -0500

    start 2021 update

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app" class="m-0 p-0 bg-gradient-to-b from-gray-700 to-gray-900 h-screen text-gray-100">
+<!--    <NavBar :routes="routes"/>-->
+    <Hero class="text-gray-100 sticky top-0"/>
+  </div>
+</template>
+
+<script>
+import Hero from "./components/Hero";
+
+export default {
+  name: 'App',
+  components: {
+    Hero,
+
+  },
+  data: () => {
+    return {
+      routes: [
+        {
+          path: "/",
+          title: "Home"
+        },
+        {
+          path: "/about",
+          title: "About"
+        },
+        {
+          path: "/resume",
+          title: "Resume"
+        },
+        {
+          path: "/contact",
+          title: "Contact"
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="w-full p-20 flex justify-center">
+  <div class="flex">
+    <img src="https://avatars.githubusercontent.com/u/11987913?v=4" class="rounded-full w-4/12 h-4/12 border-8 border-gray-10 mx-10 shadow" alt="profile picture"/>
+    <div class="">
+      <h1 class=" font-mono text-6xl">Ben Carey</h1>
+      <div class="max-w-md my-5">
+        I am a systems minded developer with a client focused mindset (I want to know my code is helping you). I am always questioning and curious with a focus on how big problems can be simplified and solutions implemented. I am lifelong student looking to expand my understanding of fundamental and complex ideas.
+      </div>
+      <div class="my-5 flex justify-end">
+        <button type="button" class="rounded-full bg-gradient-to-r from-green-400 to-blue-500 py-1 px-3 shadow ">Download my Resume</button>
+        <button type="button" class="rounded-full bg-gradient-to-r from-green-400 to-blue-500 py-1 px-3 shadow ">Download my Resume</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Hero"
+}
+</script>
+
+<style scoped>
+
+</style>
