d4b35a8391220822d18677e4db58a68a1ff15ce4
Ben Carey <bncarey42@users.noreply.github.com>
2022-07-25 09:40:59 -0500

    use new vite app instead of static using github api to access json stored in github resume repo

---
+++ src/components/resume/Resume.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="" v-if="loading">LOADING</div>
+  <div class="" v-else>
+    <employer :employer="emp" v-for="(emp, key) in work" :key="key"/>
+  </div>
+</template>
+
+<script>
+import Employer from "./Employer.vue";
+export default {
+  name: "Resume",
+  components: {Employer},
+  mounted() {
+    this.loading=true;
+    let myHeaders = new Headers({"Accept": "application/vnd.github.VERSION.raw"});
+
+    fetch("https://api.github.com/repos/bncarey42/resume/contents/resume.json?ref=main", {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    })
+        .then(response => response.json())
+        .then(data => {
+          this.work = data.work;
+          this.education = data.education;
+        })
+        .catch(error => console.log('error', error))
+        .finally(() => {
+          this.loading = false;
+        });
+  },
+  data(){
+    return {
+      work: null,
+      education: null,
+      loading: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
