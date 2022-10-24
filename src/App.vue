fc68971894d9a2e0fae3a62e94f5fe4f84cd9925
Ben Carey <bncarey42@users.noreply.github.com>
2022-10-24 13:09:13 -0500

    cleanup (#6)
    * cleanup
    
    * add transparent card to hero for readability
    
    * remove logos

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@ import Resume from "./components/resume/Resume.vue";
 import Hero from "./components/hero.vue";
 </script>
 <template>
-  <div class="m-4 lg:m-16 pill-scroll font-ubuntu">
+  <div class="m-4 lg:m-24 pill-scroll font-ubuntu">
     <div class="py-8 relative">
-      <div class="-z-10 absolute top-0 left-0 w-full h-full">
-        <div class="bg-accent-1 bg-hex h-full rounded-2xl bg-repeat-y lg:mx-10 mx-4 bg-accent-1 relative"></div>
+      <div class="-z-10 absolute top-0 left-0 w-full h-full opacity-50">
+        <div class="bg-hex bg-repeat-y h-full rounded-2xl lg:mx-10 mx-4 relative"></div>
       </div>
       <div class="">
         <hero title="Hi! I'm Ben." :msg="`I am a systems minded software engineer with a client focused mindset with ${new Date().getFullYear() - 2017} years of development and engineering experience. I am always questioning and curious
@@ -52,7 +52,7 @@ export default {
     fetch("https://api.github.com/repos/bncarey42/resume/contents/resume.json?ref=main", {
       method: 'GET',
       headers: new Headers({"Accept": "application/vnd.github.VERSION.raw"}),
-      redirect: 'follow'
+
     })
         .then(response => response.json())
         .then(data => {
@@ -67,14 +67,5 @@ export default {
 }
 </script>
 <style>
-.bg-hex {
-  background-image: url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='40' height='59.428' patternTransform='scale(3) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(211, 34%, 27%, 1)'/><path d='M0 70.975V47.881m20-1.692L8.535 52.808v13.239L20 72.667l11.465-6.62V52.808zm0-32.95l11.465-6.62V-6.619L20-13.24 8.535-6.619V6.619L20 13.24m8.535 4.927v13.238L40 38.024l11.465-6.62V18.166L40 11.546zM20 36.333L0 47.88m0 0v23.094m0 0l20 11.548 20-11.548V47.88m0 0L20 36.333m0 0l20 11.549M0 11.547l-11.465 6.619v13.239L0 38.025l11.465-6.62v-13.24L0 11.548v-23.094l20-11.547 20 11.547v23.094M20 36.333V13.24'  stroke-linejoin='round' stroke-linecap='round' stroke-width='1.5' stroke='hsla(16, 100%, 74%, 1)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>");
-  background-clip: content-box;
-}
-
-.bg-hex-sm {
-  background-image: url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='40' height='59.428' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(211, 34%, 27%, 1)'/><path d='M0 70.975V47.881m20-1.692L8.535 52.808v13.239L20 72.667l11.465-6.62V52.808zm0-32.95l11.465-6.62V-6.619L20-13.24 8.535-6.619V6.619L20 13.24m8.535 4.927v13.238L40 38.024l11.465-6.62V18.166L40 11.546zM20 36.333L0 47.88m0 0v23.094m0 0l20 11.548 20-11.548V47.88m0 0L20 36.333m0 0l20 11.549M0 11.547l-11.465 6.619v13.239L0 38.025l11.465-6.62v-13.24L0 11.548v-23.094l20-11.547 20 11.547v23.094M20 36.333V13.24'  stroke-linecap='square' stroke-width='1' stroke='hsla(16, 100%, 74%, 1)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>");
-  background-clip: content-box;
-}
 
 </style>
