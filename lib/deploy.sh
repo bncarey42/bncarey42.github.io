20980640fb859c35e922a8c4763a229e5c914342
Ben Carey <bncarey42@users.noreply.github.com>
2022-11-02 15:53:45 -0500

    can't hurt

--- lib/deploy.sh
+++ lib/deploy.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -e
 
-GITHUB_REPO=git@github.com:bncarey42/bncarey42.github.io.git
+git --version
+git config --global user.email "<>"
+git config --global user.name "bot-github-actions"
+git config --global init.defaultBranch "main"
+
+GITHUB_REPO=https://github.com/bncarey42/bncarey42.github.io.git
 GITHUB_BRANCH=main:_gh-pages
 
+npm install
 npm run build
 cd dist
 rm -rfv .git
 git init
 git add -A
-git status
 git commit -am 'New GHP Deploy'
-git status
 git push -f $GITHUB_REPO $GITHUB_BRANCH --no-verify
 cd -
