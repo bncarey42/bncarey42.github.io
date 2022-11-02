7807b4996ee639cbe0decf3a1d92247a16642d57
Ben Carey <bncarey42@users.noreply.github.com>
2022-11-02 15:36:30 -0500

    stay on target

--- lib/deploy.sh
+++ lib/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
-git config --global user.email "<>"
-git config --global user.name "bot-github-actions"
-git config --global init.defaultBranch "main"
 
 GITHUB_REPO=https://github.com/bncarey42/bncarey42.github.io.git
 GITHUB_BRANCH=main:_gh-pages
@@ -12,6 +9,8 @@ cd dist
 rm -rf .git
 git init
 git add -A
+git status
 git commit -am 'New GHP Deploy'
+git status
 git push -f $GITHUB_REPO $GITHUB_BRANCH --no-verify
 cd -
