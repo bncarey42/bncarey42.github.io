ac819a7ca24627dc8f6f853a6f4e76681d25187a
Ben Carey <bncarey42@users.noreply.github.com>
2022-11-02 16:41:08 -0500

    ?

--- lib/gh_pages_deploy.sh
+++ lib/gh_pages_deploy.sh
@@ -5,14 +5,11 @@ git --version
 git config --global user.name 'bncarey42'
 git config --global init.defaultBranch "main"
 
-GITHUB_BRANCH=_gh-pages
-
 npm ci
 npm run build
 
 cd dist
-git checkout $GITHUB_BRANCH
 git add -A
 git commit -m 'New GHP Deploy'
-git push -f --no-verify
+git push -f -u origin $GITHUB_BRANCH --no-verify
 cd -
