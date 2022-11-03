ed8a9c37bf529c023a164e1ce729df21bddbebd3
Ben Carey <bncarey42@users.noreply.github.com>
2022-11-02 20:47:45 -0500

    try this on for size

--- lib/gh_pages_deploy.sh
+++ lib/gh_pages_deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 set -e
 
-GITHUB_REPO=git@github.com:bncarey42/bncarey42.github.io.git
-GITHUB_BRANCH=main:_gh-pages
-
-mv ./dist $runner.temp/ghp-build
-
+mv ./dist $TMP_DIR/ghp-build
 rm -rf ./*
-
-mv $runner.temp/ghp-build/* .
+mv $TMP_DIR/ghp-build/* .
 
 ls -lFsgh
 
