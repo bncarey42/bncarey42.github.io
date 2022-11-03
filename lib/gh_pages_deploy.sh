29f98e38745bad21d734962031a3427bdfc1b072
Ben Carey <bncarey42@users.noreply.github.com>
2022-11-02 22:08:34 -0500

    one more?

--- lib/gh_pages_deploy.sh
+++ lib/gh_pages_deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
-
-mv ./dist $TMP_DIR/ghp-build
-rm -rf ./*
-mv $TMP_DIR/ghp-build/* .
-
-ls -lFsgh
-
+rm -rf .git
+cd ./dist
+git init
+git add -A
 git commit -am 'New GHP Deploy'
 git push -f $GITHUB_REPO $GITHUB_BRANCH --no-verify
 cd -
