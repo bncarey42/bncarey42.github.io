c1e8b281833127c043b572fdb70b81e48f62c6a8
Ben Carey <bncarey42@users.noreply.github.com>
2022-11-02 20:26:57 -0500

    this is ... somthing

--- lib/gh_pages_deploy.sh
+++ lib/gh_pages_deploy.sh
@@ -4,9 +4,13 @@ set -e
 GITHUB_REPO=git@github.com:bncarey42/bncarey42.github.io.git
 GITHUB_BRANCH=main:_gh-pages
 
-mv ${temp}
+mv ./dist $runner.temp/ghp-build
 
-cd dist
+rm -rf ./*
+
+mv $runner.temp/ghp-build/* .
+
+ls -lFsgh
 
 git commit -am 'New GHP Deploy'
 git push -f $GITHUB_REPO $GITHUB_BRANCH --no-verify
