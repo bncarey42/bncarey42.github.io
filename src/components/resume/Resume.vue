5ddfb28ea7e131ba350136095a4c36a2bf0c1295
Ben Carey <bncarey42@users.noreply.github.com>
2022-11-10 12:56:53 -0600

    reset

--- src/components/resume/Resume.vue
+++ src/components/resume/Resume.vue
@@ -1,16 +1,14 @@
 <template>
-  <card class="grid grid-cols-1 gap-3 items-center">
-    <div class="">
-      <label for="tech_filter" class="">
-        Technologies
-      </label>
-      <multiselect v-model="filter.technology" :options="technologies" descriptor="title"
-      />
-
-    </div>
-    <div class="flex justify-end"><button class="bg-secondary rounded px-2 py-1 hover:bg-opacity-80">Reset Filters</button></div>
-  </card>
-  <employer :employer="emp" v-for="(emp, key) in filtered_employers" :key="key" class="mb-5" v-if="resume"/>
+<!--  <card class="grid grid-cols-1 gap-3 items-center">-->
+<!--    <div class="">-->
+<!--      <label for="tech_filter" class="">-->
+<!--        Technologies-->
+<!--      </label>-->
+<!--      <multiselect v-model="filter.technology" :options="technologies" descriptor="title"/>-->
+<!--    </div>-->
+<!--    <div class="flex justify-end"><button class="bg-secondary rounded px-2 py-1 hover:bg-opacity-80">Reset Filters</button></div>-->
+<!--  </card>-->
+  <employer :employer="emp" :filter="filter" v-for="(emp, key) in work" :key="key" class="mb-5" v-if="resume"/>
 </template>
 
 <script>
@@ -40,42 +38,44 @@ export default {
     this.work = this.resume.work;
     this.education = this.resume.education;
   },
+  methods: {
+    reduceRoleTech(roles, existingList = []) {
+      return roles.reduce((arr, role) => {
+        if (role.technologies) {
+          role.technologies.forEach(t => {
+            if (arr.findIndex(tech => tech.title === t.title) < 0) {
+              arr.push(t)
+            }
+          })
+        }
+        if (role.projects) {
+          arr = this.reduceRoleTech(role.projects, arr)
+        }
+        return arr
+      }, existingList);
+    }
+  },
   computed: {
     filtered_employers() {
       return this.work.filter(employer => {
         let filter = [];
-        if(this.filter.employer && this.filter.employer.includes(employer.employer)) {
-          filter.push(true);
+        if(this.filter.employer) {
+          if (employer.employer.includes(this.filter.employer)) {
+            console.log(employer)
+            filter.push(true);
+          }
+          console.log(filter)
+          return filter.length > 0 ? filter.every(e=>e===true): false
         }
-        employer.roles.forEach(role => {
+        return true
 
-        })
-        return filter.every(entry => entry);
       });
     },
     technologies() {
       let employer_techs = []
+
       this.work.forEach(emp => {
-        emp.roles.forEach(role => {
-          let role_tech = []
-          if (role.technologies) {
-            role.technologies.forEach(t=> role_tech.push(t))
-          }
-          if(role.projects) {
-            role.projects.forEach(proj => {
-              if (proj.technologies) {
-                proj.technologies.forEach(t=>role_tech.push(t))
-              }
-            })
-          }
-          employer_techs = [...employer_techs, ...role_tech.reduce((arr, t)=>{
-            let idx = employer_techs.findIndex(tech=>tech.title === t.title)
-            if(idx < 0) {
-              arr.push(t)
-            }
-            return arr
-          }, [])];
-        })
+        employer_techs.push(...this.reduceRoleTech(emp.roles));
       })
       return employer_techs.sort((a, b) => a.title.localeCompare(b.title));
     }
