ab08bf62b164c7e3508b60861ea839fc1be89d09
Ben Carey <bncarey42@users.noreply.github.com>
2024-02-09 15:21:37 -0600

    align

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -13,19 +13,19 @@ defineProps({
 
 <template>
   <div class="w-full lg:py-32 lg:px-24 px-10 py-3">
-      <card class="grid lg:grid-cols-2 grid-cols-1 gap-1 items-center bg-opacity-90 py-16">
+      <card class="grid lg:grid-cols-2 grid-cols-1 gap-1 items-center bg-opacity-90 py-8">
           <div class="mb-5 lg:mb-0 w-full">
             <img src="https://avatars.githubusercontent.com/u/11987913?v=4"
-                 class="rounded-full w-40 h-40 lg:w-80 lg:h-80 border border-4 border-secondary m-auto lg:ml-1/4 shadow"
+                 class="rounded-full w-40 h-40 lg:w-96 lg:h-96 border-4 border-secondary m-auto lg:ml-1/4 shadow"
                  alt="Profile Avatar for Ben Carey">
           </div>
           <div class="">
-            <div class="w-full p-3 rounded-lg">
+            <div class="w-full rounded-lg">
               <h1>{{title}}</h1>
-              <div class="px-3">
+              <div class="mb-3` lg:pr-28">
                 <p>{{ msg }}</p>
               </div>
-              <div class="flex justify-center lg:justify-end gap-3 items-center text-xl">
+              <div class="flex justify-center lg:justify-start gap-3 items-center text-2xl">
                 <p>Contact me at:</p>
                 <a v-for="(site, key) in links" :key="key"
                    :href="site.link" target="_blank"
