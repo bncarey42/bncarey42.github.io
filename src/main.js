e41ea5f75d6f35c4fc88d6c8686b7af0fc209190
Ben Carey <bncarey42@users.noreply.github.com>
2024-02-09 13:09:36 -0600

    testing global Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
 import App from './App.vue'
 
 
 import './style.css'
 import 'devicon/devicon.min.css'
 
-createApp(App).mount('#app')
+Vue.createApp(App).mount('#app')
